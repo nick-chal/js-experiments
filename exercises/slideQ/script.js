959f59575ae5ec2898bf75138520d067463b93e0
nickchal <nickguy.s@gmail.com>
2018-11-26 22:42:31 +0545

    object search exercise

---
+++ exercises/slideQ/script.js
@@ -0,0 +1,42 @@
+var fruits = [{
+    id: 1,
+    name: 'Banana',
+    color: 'Yellow'
+  },
+  {
+    id: 2,
+    name: 'Apple',
+    color: 'Red'
+  },
+  {
+    id: 3,
+    name: 'Orange',
+    color: 'Orange'
+  },
+
+];
+
+function searchByName(arr, searchName) {
+  var result;
+  arr.forEach(function (val) {
+    if (val.name == searchName) {
+      result = val;
+    }
+  });
+  return result;
+}
+
+function searchByKey(arr, searchKey, searchItem) {
+  var result;
+  arr.forEach(function (val) {
+    if (val[searchKey] == searchItem) {
+      result = val;
+    }
+  });
+  return result;
+}
+
+console.log(searchByName(fruits, 'Apple'));
+
+
+console.log(searchByKey(fruits, 'color', 'Orange'));
