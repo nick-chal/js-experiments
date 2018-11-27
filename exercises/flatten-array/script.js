eb5497ae72947a5666417e3f78df7f63631ba47b
nickchal <nickguy.s@gmail.com>
2018-11-27 14:22:17 +0545

    flatten recursive

--- exercises/flatten-array/script.js
+++ exercises/flatten-array/script.js
@@ -1,16 +1,20 @@
-var data = [[1, 2, 3], [2, 4, 5], 6, [2,3,4,7,1]];
+var data = [
+  [1, 2, 3],
+  [2, 4, 5], 6, [2, [3, 10, [15, [15, 21, 23], 22], 36], 7, 1]
+];
 
-var answer = data.reduce(function (arr, val) {
-  if(Array.isArray(val)){
-    val.forEach(function(item) {
-      if(arr.indexOf(item) == -1){
-        arr.push(item)
-      };
-    });
-  } else if(arr.indexOf(val) == -1 ){
-    arr.push(val);
-  }
-  return arr;
-},[]);
+function flattenArray(data, acc) {
+  var result = data.reduce(function (arr, val) {
+    if (Array.isArray(val)) {
+      arr = flattenArray(val, arr);
+    } else if (arr.indexOf(val) == -1) {
+      arr.push(val);
+    }
+    return arr;
+  }, acc);
+  return result;
+}
+
+var answer = flattenArray(data, []);
 
 console.log(answer);
