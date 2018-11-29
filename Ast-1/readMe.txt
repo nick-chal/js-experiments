f62014ffc34e6457be0336a27b08da51683b4091
nickchal <nickguy.s@gmail.com>
2018-11-29 22:08:04 +0545

    final version, library implemented

---
+++ Ast-1/readMe.txt
@@ -0,0 +1,10 @@
+An Image Carousel!
+Keep you images in the format:
+  div>(any number of images)
+Everything else will be added automatically
+
+you can change the style using carousel.css 
+the height width of carousel can be changed through class "container-wrapper"
+
+select the div containing images and pass it to the object constructor
+call objectName.doccumentInit() to start the movement
--- Ast-1/script.js
+++ Ast-1/script.js
@@ -1,126 +1,92 @@
-var slider = document.getElementsByClassName('slider')[0];
-// var rightArrow = document.getElementById('right-arrow');
-// var leftArrow = document.getElementById('left-arrow');
-// var navButtons = document.getElementsByClassName('dot-container')[0].getElementsByTagName('span');
+var sliderDiv = document.getElementsByClassName('slider')[0];
 
-function Carousel(element, width, height) {
-	this.slider = element;
-	this.sliderParent = slider.parentNode;
-	this.sliderContainer = document.createElement('div');
-	this.width = width;
-	this.height = height;
-	this.images = this.slider.getElementsByTagName('img');
-	this.imageNumber = this.images.length;
-	this.rightArrow = document.createElement('img');
-	this.leftArrow = document.createElement('img');
-	this.dotContainer = document.createElement('div');
-	this.navButtons = [];
-	that = this;
+function Carousel(element) {
+	var slider = element;
+	var sliderParent = slider.parentNode;
+	var sliderContainer = document.createElement('div');
+	var width;
+	var height;
+	var images = slider.getElementsByTagName('img');
+	var imageNumber = images.length;
+	var rightArrow = document.createElement('img');
+	var leftArrow = document.createElement('img');
+	var dotContainer = document.createElement('div');
+	var navButtons = [];
 
-	var imageWidth = that.width;
+	var imageWidth;
 	var x = 0;
-	var speed = 5;
+	var speed;
 	var delta = 1;
-	var navIndex;
 
 	var mainTimeout;
 	var buttonTimeInterval;
 	var mainInterval;
 
+	function setWidthAndHeight() {
+		width = sliderContainer.offsetWidth;
+		height = sliderContainer.offsetHeight;
+		imageWidth = width;
+	}
+
 	function createAll() {
-		this.sliderContainer = document.createElement('div');
-		this.rightArrow = document.createElement('img');
-		this.leftArrow = document.createElement('img');
-		this.dotContainer = document.createElement('div');
-		that.sliderParent.removeChild(that.slider);
-		that.sliderContainer.appendChild(that.slider);
-		that.sliderContainer.appendChild(that.dotContainer);
-		that.sliderContainer.appendChild(that.leftArrow);
-		that.sliderContainer.appendChild(that.rightArrow);
-		that.sliderParent.appendChild(that.sliderContainer);
+		sliderContainer = document.createElement('div');
+		rightArrow = document.createElement('img');
+		leftArrow = document.createElement('img');
+		dotContainer = document.createElement('div');
+		sliderContainer.classList.add('container-wrapper');
+		rightArrow.classList.add('right-arrow');
+		leftArrow.classList.add('left-arrow');
+		dotContainer.classList.add('dot-container');
+		sliderParent.removeChild(slider);
+		sliderContainer.appendChild(slider);
+		sliderContainer.appendChild(dotContainer);
+		sliderContainer.appendChild(leftArrow);
+		sliderContainer.appendChild(rightArrow);
+		sliderParent.appendChild(sliderContainer);
 	}
 
 	function dotInit() {
-		that.dotContainer.style.position = 'absolute';
-		that.dotContainer.style.bottom = '5%';
-		that.dotContainer.style.left = '45%';
-		for (var i = 0; i < that.imageNumber; i++) {
+		for (var i = 0; i < imageNumber; i++) {
 			var spanElement = document.createElement('span');
-			spanElement.style.margin = "5px";
-			spanElement.style.width = '10px';
-			spanElement.style.height = '10px';
-			spanElement.style.borderRadius = '50%';
-			spanElement.style.display = 'inline-block';
-			spanElement.style.backgroundColor = 'darkgray';
-			spanElement.setAttribute('class', 'nav-button' + i);
-			that.dotContainer.appendChild(spanElement);
-			that.navButtons.push(spanElement);
+			spanElement.classList.add('nav-button' + i);
+			dotContainer.appendChild(spanElement);
+			navButtons.push(spanElement);
 			spanElement.addEventListener('click', function (e) {
 				navSlide(e);
 			});
 		}
 	}
 
-	function slideContainerStyler() {
-		that.sliderContainer.style.width = width + 'px';
-		that.sliderContainer.style.height = height + 'px';
-		that.sliderContainer.style.overflow = 'hidden';
-		// this.sliderContainer.style.boxShadow
-		that.sliderContainer.style.position = 'relative';
-	}
-
 	function sliderStyler() {
-		that.slider.style.position = 'absolute';
-		that.slider.style.height = '100%';
-		that.slider.style.width = (that.imageNumber * that.width) + 'px';
+		slider.style.width = (imageNumber * width) + 'px';
 	}
 
 	function imgStyler() {
-		for (var i = 0; i < that.imageNumber; i++) {
-			that.images[i].style.height = that.height + 'px';
-			that.images[i].style.width = that.width + 'px'
-			that.images[i].style.float = 'left';
-			that.images[i].style.display = 'block';
+		for (var i = 0; i < imageNumber; i++) {
+			images[i].style.height = height + 'px';
+			images[i].style.width = width + 'px';
 		}
 	}
 
 	function arrowStyler() {
-		that.leftArrow.style.position = 'absolute';
-		that.leftArrow.style.left = '0';
-		that.leftArrow.style.top = '44%';
-		that.leftArrow.style.height = '50px';
-		that.leftArrow.style.width = '50px';
-		that.leftArrow.src = './images/left-arrow.png';
-		that.leftArrow.style.cursor = 'pointer';
-		that.rightArrow.style.position = 'absolute';
-		that.rightArrow.style.right = '0';
-		that.rightArrow.style.top = '44%';
-		that.rightArrow.style.height = '50px';
-		that.rightArrow.style.width = '50px';
-		that.rightArrow.src = './images/right-arrow.png';
-		that.rightArrow.style.cursor = 'pointer';
+		leftArrow.src = './images/left-arrow.png';
+		rightArrow.src = './images/right-arrow.png';
 	}
 
-	that.rightArrow.addEventListener('click', function () {
-		buttonSlide(1);
-	});
-
-	that.leftArrow.addEventListener('click', function () {
-		buttonSlide(-1);
-	})
-
-	for (var i = 0; i < that.navButtons.length; i++) {
-		that.navButtons[i].addEventListener('click', function (e) {
+	for (var i = 0; i < navButtons.length; i++) {
+		navButtons[i].addEventListener('click', function (e) {
 			navSlide(e);
-		})
+		});
 	}
+
+	var positionLeft;
 
 	function navSlide(event) {
 		clearInterval(mainInterval);
 		clearInterval(buttonTimeInterval);
 		clearTimeout(mainTimeout);
-		speed = 10;
 		navIndexPosition = (parseInt(event.target.className.slice('10', event.target.className.length))) * imageWidth;
+		positionLeft = x > navIndexPosition;
 
 		if (navIndexPosition === x) {
 			initiateMovement(2000);
@@ -128,11 +94,11 @@ function Carousel(element, width, height) {
 			if (navIndexPosition < x) delta = -1;
 			else delta = 1;
 			buttonTimeInterval = setInterval(function () {
-
 				slider.style.left = '-' + x + 'px';
-				if (x === navIndexPosition) {
+				checkActiveButton();
+				if (positionLeft !== (x > navIndexPosition) || x === navIndexPosition) {
 					clearInterval(buttonTimeInterval);
-					initiateMovement(((x != 0) && (x != imageWidth * (that.imageNumber - 1)) ? 2000 : 000));
+					initiateMovement(((x != 0) && (x != imageWidth * (imageNumber - 1)) ? 2000 : 000));
 				} else {
 					x += (speed * delta);
 				}
@@ -147,8 +113,7 @@ function Carousel(element, width, height) {
 		clearInterval(mainInterval);
 		clearInterval(buttonTimeInterval);
 		clearTimeout(mainTimeout);
-
-		if ((x >= imageWidth * (that.imageNumber - 1) && delta === 1) || (x === 0 && delta === -1)) {
+		if ((x >= imageWidth * (imageNumber - 1) && delta === 1) || (x === 0 && delta === -1)) {
 			clearInterval(mainInterval);
 			initiateMovement(0);
 		} else {
@@ -156,12 +121,11 @@ function Carousel(element, width, height) {
 		}
 	}
 
-
 	function slide() {
 		slider.style.left = '-' + x + 'px';
 		x += (speed * delta);
 
-		if (x >= imageWidth * (that.imageNumber - 1)) delta = -1;
+		if (x >= imageWidth * (imageNumber - 1)) delta = -1;
 		else if (x <= 0) delta = 1;
 
 		if (x % imageWidth === 0) {
@@ -171,26 +135,43 @@ function Carousel(element, width, height) {
 		}
 	}
 
+	function checkActiveButton() {
+		for (i = 0; i < imageNumber; i++) {
+			if ((x / imageWidth) === i) navButtons[i].classList.add('active-button');
+			else navButtons[i].classList.remove('active-button');
+		}
+	}
+
 	function initiateMovement(timeLimit) {
-		speed = 5;
+		checkActiveButton();
 		mainTimeout = setTimeout(function () {
 			mainInterval = setInterval(slide, 4);
 		}, timeLimit);
 	}
 
-	that.documentInit = function () {
+	this.documentInit = function () {
 		createAll();
 		dotInit();
-		slideContainerStyler();
-		console.log(that.sliderContainer);
+		setWidthAndHeight();
 		sliderStyler();
-		imgStyler();
 		arrowStyler();
-		console.log(that.sliderContainer);
+		imgStyler();
+
+		speed = 7 - (width % 7);
+
+		rightArrow.addEventListener('click', function () {
+			buttonSlide(1);
+		});
+
+		leftArrow.addEventListener('click', function () {
+			buttonSlide(-1);
+		});
+
+		checkActiveButton();
 		initiateMovement(2000);
 	}
 }
 
-var carousel = new Carousel(slider, 400, 400);
+var carousel = new Carousel(sliderDiv);
 
 carousel.documentInit();
